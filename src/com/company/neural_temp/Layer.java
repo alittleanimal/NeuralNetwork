b59f91d6e323c737998b822e0588587dbb75baf4
alittleanimal <741996133@qq.com>
2019-12-01 16:14:15 +0800

    First chapter

---
+++ src/com/company/neural_temp/Layer.java
@@ -0,0 +1,24 @@
+package com.company.neural_temp;
+
+import java.util.ArrayList;
+
+public abstract class Layer {
+    private ArrayList<Neuron> listOfNeurons;
+    private int numberOfNeuronsInLayer;
+
+    public ArrayList<Neuron> getListOfNeurons() {
+        return listOfNeurons;
+    }
+
+    public void setListOfNeurons(ArrayList<Neuron> listOfNeurons) {
+        this.listOfNeurons = listOfNeurons;
+    }
+
+    public int getNumberOfNeuronsInLayer() {
+        return numberOfNeuronsInLayer;
+    }
+
+    public void setNumberOfNeuronsInLayer(int numberOfNeuronsInLayer) {
+        this.numberOfNeuronsInLayer = numberOfNeuronsInLayer;
+    }
+}
---
+++ src/com/company/neural_temp/NeuralNetTest.java
@@ -0,0 +1,9 @@
+package com.company.neural_temp;
+
+public class NeuralNetTest {
+    public static void main(String[] args) {
+        NeuralNet n = new NeuralNet();
+        n.initNet();
+        n.printNet();
+    }
+}
